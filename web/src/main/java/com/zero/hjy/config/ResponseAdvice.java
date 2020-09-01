f9c3366d5f328cf8327e395a20eea26aa0bc9d8e
zero <1428499659zero@gmail.com>
2020-09-01 16:17:17 +0800

    统一处理response参数格式

---
+++ web/src/main/java/com/zero/hjy/config/ResponseAdvice.java
@@ -0,0 +1,57 @@
+package com.zero.hjy.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+import java.lang.reflect.Method;
+
+/**
+ * 设置controller response参数格式
+ */
+@RestControllerAdvice
+@SuppressWarnings("rawtypes")
+@Configuration
+public class ResponseAdvice implements ResponseBodyAdvice {
+
+    /**
+     * 拦截策略
+     *
+     * @param returnType    the return type
+     * @param converterType the selected converter type
+     * @return {@code true} if {@link #beforeBodyWrite} should be invoked;
+     * {@code false} otherwise
+     */
+    @Override
+    public boolean supports(MethodParameter returnType, Class converterType) {
+        Method method = returnType.getMethod();
+        String methodName = method.getName();
+        // 处理忽略拦截的方法
+        if ("login".equals(methodName)) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Invoked after an {@code HttpMessageConverter} is selected and just before
+     * its write method is invoked.
+     *
+     * @param body                  the body to be written
+     * @param returnType            the return type of the controller method
+     * @param selectedContentType   the content type selected through content negotiation
+     * @param selectedConverterType the converter type selected to write to the response
+     * @param request               the current request
+     * @param response              the current response
+     * @return the body that was passed in or a modified (possibly new) instance
+     */
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+        return body;
+    }
+}
